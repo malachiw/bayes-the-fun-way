0aa1016878b232ada559c739c4e6f29384bebf97
malachiw <malachi.woodlee@gmail.com>
2026-01-02 23:07:44 -0500

    p3 and solutions

---
+++ Chapter2 Q3.R
@@ -0,0 +1,14 @@
+# Measuring Uncertainty
+# The Yankess are playing the Red Sox. You're a diehard Sox fan and bet your 
+# friend they'll win the game. You'll pay your friend $30 if the Sox lose and
+# your friend will have to pay you only $5 if the Sox win. What is the 
+# probability you have intuitively assigned to the belief that the Red Sox will
+# win?
+
+# P(win)/P(lose) = 30/5 = 6
+# P(win) = 6*P(lose)
+# P(win) = 6*(1-P(win))
+# P(win) = 6*6-P(win)
+# 7*P(win) = 6
+6/7 # = P(win)
+
