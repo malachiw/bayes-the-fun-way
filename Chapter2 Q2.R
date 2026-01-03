d07e3c952570d8c91b2aefdd7fd9c88687b27698
malachiw <malachi.woodlee@gmail.com>
2026-01-02 22:25:55 -0500

    finished writing out exercise

--- Chapter2 Q2.R
+++ Chapter2 Q2.R
@@ -1,2 +1,12 @@
-sum(apply(expand.grid(c(1:6),c(1:6),c(1:6)),1,sum) > 7)
+# Chapter 2 Measuring Uncertainty
+# What is the probability of rolling three six-sided dice and getting a value 
+# greater than 7?
 
+# Number of desired outcomes 
+print(sum(apply(expand.grid(c(1:6),c(1:6),c(1:6)),1,sum) > 7)) # = 181
+
+# Total outcomes
+print(6^3) # 216
+
+# P(val>7)
+print(181/216*100)
